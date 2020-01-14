8819e14eace8ca4bf95044c5c08b891753e44647
zen37 <mobi2009ro@gmail.com>
2020-01-13 17:01:51 -0800

    Create clock_dial.dart

---
+++ clock_contest/myclock/clock_dial.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import './clock_text.dart';
+import './clock_dial.dart';
+import './clock_hands.dart';
+
+
+class ClockFace extends StatelessWidget{
+
+  final DateTime dateTime;
+  final ClockText clockText;
+
+  ClockFace({this.clockText = ClockText.arabic, this.dateTime});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+     padding: const EdgeInsets.all(10.0),
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.white,
+          ),
+
+          child: Stack(
+            children: <Widget>[
+              //dial and numbers
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                padding: const EdgeInsets.all(10.0),
+                child: CustomPaint(
+                  painter: ClockDial(clockText: clockText),
+                ),
+              ),
+              ClockHands(dateTime:dateTime),
+
+            ],
+          ),
+        ),
+
+      ),
+    );
+  }
+}
