b3cd6763fe34eb60a3f19da500b57a944eed36d3
zen37 <mobi2009ro@gmail.com>
2020-01-15 17:20:20 -0800

    Create style.dart

---
+++ clock_contest/myclock/style.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const String FontNameDefault = 'Montserrat';
+
+const NumberStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontSize: 22,
+  color: Colors.black87,
+);
+
+const HourHandColor = Colors.black87;
+const HourHandThickness = 4.0;
+const HourHandSize = 0.4;
+
+const MinuteHandColor = Colors.black87;
+const MinuteHandThickness = 4.0;
+const MinuteHandSize = 0.8;
