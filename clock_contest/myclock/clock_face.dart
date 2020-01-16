b62377a35eeda23806dc6dcdc824b9863d6e6cd2
zen37 <mobi2009ro@gmail.com>
2020-01-15 17:17:53 -0800

    Update clock_face.dart

--- clock_contest/myclock/clock_face.dart
+++ clock_contest/myclock/clock_face.dart
@@ -1,47 +1,39 @@
 import 'package:flutter/material.dart';
+import 'clock_dial.dart';
+import 'clock_hands.dart';
 
-import './clock_text.dart';
-import './clock_dial.dart';
-import './clock_hands.dart';
-
-
-class ClockFace extends StatelessWidget{
-
+class ClockFace extends StatelessWidget {
   final DateTime dateTime;
-  final ClockText clockText;
 
-  ClockFace({this.clockText = ClockText.arabic, this.dateTime});
+  ClockFace({this.dateTime});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-     padding: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.all(5.0),
       child: AspectRatio(
         aspectRatio: 1.0,
         child: Container(
           width: double.infinity,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
-            color: Colors.white,
+            color: Colors.transparent,
           ),
-
           child: Stack(
             children: <Widget>[
               //dial and numbers
               Container(
                 width: double.infinity,
                 height: double.infinity,
-                padding: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(5.0),
                 child: CustomPaint(
-                  painter: ClockDial(clockText: clockText),
+                  painter: ClockDial(),
                 ),
               ),
-              ClockHands(dateTime:dateTime),
-
+              ClockHands(dateTime: dateTime),
             ],
           ),
         ),
-
       ),
     );
   }
