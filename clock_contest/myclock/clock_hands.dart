09b0a0ab16c2ecd98341cea85d2d2d106fa0fbb1
zen37 <mobi2009ro@gmail.com>
2020-01-15 17:18:15 -0800

    Update clock_hands.dart

--- clock_contest/myclock/clock_hands.dart
+++ clock_contest/myclock/clock_hands.dart
@@ -1,15 +1,13 @@
-//import 'dart:async';
-
 import 'package:flutter/material.dart';
-
 import 'package:vector_math/vector_math_64.dart' show radians;
 import 'drawn_hand.dart';
+import 'style.dart';
 
 /// Total distance traveled by a second or a minute hand, each second or minute, respectively.
-final radiansPerTick = radians(360 / 60);
+final radiansPerTick = radians(6);
 
 /// Total distance traveled by an hour hand, each hour, in radians.
-final radiansPerHour = radians(360 / 12);
+final radiansPerHour = radians(30);
 
 class ClockHands extends StatelessWidget {
   final DateTime dateTime;
@@ -18,27 +16,26 @@ class ClockHands extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new AspectRatio(
+    return AspectRatio(
       aspectRatio: 1.0,
-      child: new Container(
+      child: Container(
         width: double.infinity,
         //padding: const EdgeInsets.all(20.0),
-        child: new Stack(
+        child: Stack(
           fit: StackFit.expand,
           children: <Widget>[
             DrawnHand(
-              color: Colors.grey,
-              thickness: 5, // zm 16,
-              size: 0.5,
+              color: HourHandColor,
+              thickness: HourHandThickness,
+              size: HourHandSize,
               angleRadians: dateTime.hour * radiansPerHour +
                   (dateTime.minute / 60) * radiansPerHour,
             ),
             DrawnHand(
-              color: Colors.redAccent,
-              thickness: 4, // zm 16,
-              size: 0.8,
-              angleRadians: dateTime.minute *
-                  radiansPerTick, 
+              color: MinuteHandColor,
+              thickness: MinuteHandThickness,
+              size: MinuteHandSize,
+              angleRadians: dateTime.minute * radiansPerTick,
             ),
           ],
         ),
